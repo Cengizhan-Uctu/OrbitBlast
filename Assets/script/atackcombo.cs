7587ec95b79020d70c0aec1f5a70043cb58db93d
Cengizhan Uçtu <cengizhan_894@hotmail.com>
2021-05-15 16:11:49 +0300

    c

---
+++ Assets/script/atackcombo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atackcombo : MonoBehaviour
+{
+    Animator anim;
+   
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("attack",true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("attack",false);
+
+        }
+
+    }
+}
+
+
---
+++ Assets/script/playermove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    float inputx;
+    float inputy;
+    Vector3 playermotor;
+    Animator anim;
+    float maxlong;
+    float rotatespeed;
+    float speed=2;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputx = Input.GetAxis("Horizontal");
+        inputy = Input.GetAxis("Vertical");
+        movemotor();
+        rotaremotor();
+    }
+    public void movemotor()
+    {
+        anim.SetFloat("Horizontal",inputx);
+        anim.SetFloat("Vertical", inputy);
+        playermotor = new Vector3(inputx,0,inputy) *Time.deltaTime*speed;
+        transform.position += playermotor;
+    }
+    public void rotaremotor()
+    {
+
+    }
+}
